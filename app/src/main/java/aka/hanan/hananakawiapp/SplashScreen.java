efddff591d97b2a096d94919f189965079a59ee4
hanan <hnan.akawi@gmail.com>
2023-11-06 14:01:36 +0200

    123232264تنتم4

--- app/src/main/java/aka/hanan/hananakawiapp/SplashScreen.java
+++ app/src/main/java/aka/hanan/hananakawiapp/SplashScreen.java
@@ -2,7 +2,9 @@ package aka.hanan.hananakawiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        Handler h=new Handler();
+        Runnable r=new Runnable() {
+            public void run()
+            {
+                //to open new activity from current to next
+                Intent i= new Intent(SplashScreen.this,SignIn.class);
+                startActivity(i);
+                //to close current activity
+                finish();
+            }
+        };
+        h.postDelayed(r,3000);
     }
-}
+    }
